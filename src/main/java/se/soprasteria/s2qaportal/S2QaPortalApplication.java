ecd12cf6336ebb9e8b2c52788153c0d96f7c56b0
Mikael Elofsson <mikael.elofsson@soprasteria.com>
2019-02-01 13:43:19 +0100

    Added project

---
+++ src/main/java/se/soprasteria/s2qaportal/S2QaPortalApplication.java
@@ -0,0 +1,14 @@
+package se.soprasteria.s2qaportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2QaPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S2QaPortalApplication.class, args);
+    }
+
+}
+
