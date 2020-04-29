d997c527a94000098d7fe0fd85e98c3bd6bba6a3
Mikael Elofsson <mikael.elofsson@telenor.se>
2020-04-29 12:23:42 +0200

    deleted hard coded results

--- src/main/java/se/soprasteria/s2qaportal/controller/BaseController.java
+++ src/main/java/se/soprasteria/s2qaportal/controller/BaseController.java
@@ -20,23 +20,6 @@ public class BaseController implements ApplicationListener<ContextRefreshedEvent
 
     private void initData(){
         testRepository.deleteAll();
-
-        TestCase testCase1 = new TestCase("storeStartRentedSection","Error when trying to find an element by text [ Thriller ]","DemoBrowser");
-
-        TestCase testCase2 = new TestCase("storeAssetDetailForEpisodeLoggedOut","Error when trying to find an element by text [ Tv-serier ]","DemoBrowser");
-
-        TestCase testCase3 = new TestCase("searchOverlay","Example logMessage of a test 3 that has never existed","DemoBrowser");
-        TestCase testCase4 = new TestCase("storeProviderFilterLoggedOut","Example logMessage of a test 4 that has never existed","DemoBrowser");
-
-        testCase1.setFullName("telia.playplus.crossplatform.videoOnDemand.store.StoreStart.storeStartRentedSection");
-        testCase2.setFullName("telia.playplus.crossplatform.videoOnDemand.store.StoreAssetDetailForEpisode.storeAssetDetailForEpisodeLoggedOut");
-        testCase3.setFullName("telia.playplus.crossplatform.search.SearchFunctions.searchOverlay");
-        testCase4.setFullName("telia.playplus.crossplatform.videoOnDemand.store.StoreProviderFilter.storeProviderFilterLoggedOut");
-
-        testRepository.save(testCase1);
-        testRepository.save(testCase2);
-        testRepository.save(testCase3);
-        testRepository.save(testCase4);
     }
 
 }
